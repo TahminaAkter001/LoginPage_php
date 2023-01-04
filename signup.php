66b6522aafcc5fbc1eda24da8cdf7c68028bcf57
Tahmina6023 <tahminatamanna799@gmail.com>
2023-01-05 03:05:05 +0600

    2nd push

---
+++ signup.php
@@ -0,0 +1,57 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp Page</title>
+</head>
+<body>
+
+
+<style type="text/css">
+    #text{
+        height:25px;
+        border-radius:5px;
+        padding:4px;
+        border:solid thin #aaa;
+        width:100%;
+    }
+    #button{
+        padding:10px;
+        width:100px;
+        color:white;
+        background-color:lightblue;
+        border:none;
+        font-weight:bold;
+    }
+    #box{
+        background-color:grey;
+        margin:none;
+        width:300px;
+        padding:20px;
+        margin:auto;
+    }
+</style>
+
+<div id="box">
+    <form method="post">
+<div style="font-size: 26px; font-weight: bold; margin: 10px; color: white;">
+    Signup
+</div>
+
+        <input id="text" type="text" name="username" placeholder="Enter Username"><br><br>
+        <input id="text" type="password" name="password" placeholder="Enter Password"><br><br>
+        <input id="button" type="submit" value="SignUp"><br><br>
+
+        <a href="login.php">Click to Login</a><br><br>
+        </form>
+   
+</div>
+    
+</body>
+</html>
