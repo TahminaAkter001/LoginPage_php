cb806d51d39706bef54b3f86da5b30f47bd615b5
Tahmina6023 <tahminatamanna799@gmail.com>
2023-01-05 04:34:45 +0600

    signup and login

--- connection.php
+++ connection.php
@@ -1,9 +1,12 @@
 <?php
+
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpassword = "";
+$dbpass = "";
 $dbname = "login_sample_db";
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpassword,$dbname)){
-    die("Feiled to connect");
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+
+	die("failed to connect!");
 }
--- index.php
+++ index.php
@@ -1,22 +1,24 @@
-<?php
+<?php 
 session_start();
-$_SESSION;
+
+	include("connection.php");
+	include("functions.php");
+
+	$user_data = check_login($con);
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My website</title>
+	<title>My website</title>
 </head>
 <body>
-    <a href="logout.php">logout</a>
-    <h1>This is the index page!</h1>
 
-    <br>
-    Hello, username.
+	<a href="logout.php">Logout</a>
+	<h1>This is the index page</h1>
+
+	<br>
+	Hello, <?php echo $user_data['user_name']; ?>
 </body>
 </html>
