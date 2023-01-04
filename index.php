cd6091fd8f123c46bf3b709532e2449170628397
Tahmina6023 <tahminatamanna799@gmail.com>
2023-01-05 02:27:11 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+</head>
+<body>
+    <a href="logout.php">logout</a>
+    <h1>This is the index page!</h1>
+
+    <br>
+    Hello, username.
+</body>
+</html>
